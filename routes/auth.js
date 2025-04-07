5e0532daab11c7fe7b81da3bbe811e76f628a3b9
shahana .j <shahana2003J@gmail.com>
2025-04-07 18:38:30 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
